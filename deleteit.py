a87402bd07986d7f05810d8a3310c3432286d1cd
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-03-14 13:05:45 +0900

    thumbnails

--- deleteit.py
+++ deleteit.py
@@ -1,2 +1,14 @@
-import tensorflow as tf
-print(tf.__version__)
+import cv2 as cv
+import sys
+ 
+img = cv.imread(cv.samples.findFile("starry_night.jpg"))
+ 
+if img is None:
+    sys.exit("Could not read the image.")
+ 
+cv.imshow("Display window", img)
+k = cv.waitKey(0)
+ 
+if k == ord("s"):
+    cv.imwrite("starry_night.png", img)
+ 
